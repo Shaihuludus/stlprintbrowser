ed3866dca6aa3d5390839960a9b5100ce3184017
Daniel Madejek <dmadejek@gmail.com>
2021-06-06 21:33:57 +0200

    refactor start window

--- stlprintbrowser/stl_print_browser.py
+++ stlprintbrowser/stl_print_browser.py
@@ -8,12 +8,20 @@ settings = Settings()
 database = STLDatabase(settings)
 
 sg.theme(settings.theme)
-main_window = MainWindow(database.get_stl_models());
-window = sg.Window('STL PRINT BROWSER', main_window.layout, finalize=True)
-if len(main_window.models) > 0:
-    window['-MODELS_TABLE-'].update(select_rows=[0])
-    if len(main_window.selected_row.images) > 0:
-        window['-MODEL_IMAGE-'].update(data = ImageTk.PhotoImage(Image.open(main_window.selected_row.images[0])))
+main_window = MainWindow(database.get_stl_models())
+
+
+def start_window():
+    start = sg.Window('STL PRINT BROWSER', main_window.layout, finalize=True)
+    if len(main_window.models) > 0:
+        start['-MODELS_TABLE-'].update(select_rows=[0])
+        if len(main_window.selected_row.images) > 0:
+            start['-MODEL_IMAGE-'].update(data=ImageTk.PhotoImage(
+                Image.open(main_window.selected_row.images[0])))
+    return start
+
+
+window = start_window()
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
