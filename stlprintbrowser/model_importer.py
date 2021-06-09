791c845c7d5975fec8721a1db317b6391e718808
Daniel Madejek <dmadejek@gmail.com>
2021-06-09 23:06:32 +0200

    fix for file extension in importer

--- stlprintbrowser/model_importer.py
+++ stlprintbrowser/model_importer.py
@@ -25,9 +25,9 @@ def read_directory(directory, model):
         if path.isfile(directory+file):
             file_splitted = path.basename(file).split('.')
             if len(file_splitted) > 0:
-                if file_splitted[1] in image_types:
+                if file_splitted[-1] in image_types:
                     model.images.append(path.abspath(directory + file).replace('\\', '/'))
-                if file_splitted[1] in model_types:
+                if file_splitted[-1] in model_types:
                     model.filenames.append(path.abspath(directory + file).replace('\\', '/'))
         if path.isdir(directory+file):
             read_directory(directory+file+'/',model)
