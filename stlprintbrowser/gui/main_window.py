e9924f0a1f1d1fdacf29b2c3a011a38f18a3f75c
Daniel Madejek <dmadejek@gmail.com>
2021-07-31 19:28:37 +0200

    added image showing

--- stlprintbrowser/gui/main_window.py
+++ stlprintbrowser/gui/main_window.py
@@ -1,4 +1,5 @@
 from kivy.metrics import dp
+from kivy.uix.image import Image
 from kivymd.app import MDApp
 from kivymd.uix.datatables import MDDataTable
 from kivymd.uix.menu import MDDropdownMenu
@@ -17,21 +18,34 @@ class MainWindowController():
         authors.add('All')
         return authors
 
+    def loadImage(self, id):
+        model = self.models[id]
+        if(len(model.images)>0):
+            return model.images[0]
+        return 'no_image_available.png'
+
+    def loadImages(self, id):
+        model = self.models[id]
+        if(len(model.images)>0):
+            return model.images
+        return {'no_image_available.png'}
+
 class MainApp(MDApp):
 
-    def __init__(self,mainWindowController):
+    def __init__(self,main_window_controller):
         MDApp.__init__(self)
-        self.mainWindowController = mainWindowController
+        self.main_window_controller = main_window_controller
 
     def build(self):
-        self.root.ids.authors_filter.values = self.mainWindowController.authors
-        self.root.ids.table_part.add_widget(self.prepare_table())
+        self.root.ids.authors_filter.values = self.main_window_controller.authors
+        self.models_table = self.prepare_table()
+        self.root.ids.table_part.add_widget(self.models_table)
         self.prepare_table_menu()
         self.root.ids.rail.anim_color_active(self.root.ids.nav_table_item)
 
 
     def prepare_table(self):
-        return MDDataTable(
+        models_table =  MDDataTable(
             size_hint=(1,1),
             pos_hint={'center_x': .5,'center_y': .5},
             check=True,
@@ -41,15 +55,27 @@ class MainApp(MDApp):
                 ("Name", dp(70)),
                 ("Directory", dp(110)),
                 ("Printed", dp(20)),
-                ("Author", dp(50))
+                ("Author", dp(50)),
+                ("", dp(0))
             ],
             row_data = self.prepare_rows()
         )
+        models_table.bind(on_row_press = self.on_row_press)
+        return models_table
+
+    def on_row_press(self, instance_table, instance_row):
+        '''Called when a table row is clicked. index%column_data.length'''
+        index = int(instance_row.table.data_model.data[(instance_row.index - instance_row.index%len(instance_table.column_data))+len(instance_table.column_data)-1]['text'])
+        self.root.ids.preview_image.source = self.main_window_controller.loadImage(index)
+        images = self.main_window_controller.loadImages(index)
+        self.root.ids.preview_image_c.clear_widgets()
+        for path in images:
+            self.root.ids.preview_image_c.add_widget(Image(source = path))
 
     def prepare_rows(self):
         data = []
-        for model in self.mainWindowController.models:
-            single_entry = (model.name, model.directory, model.printed, model.author)
+        for index,model in enumerate(self.main_window_controller.models):
+            single_entry = (model.name, model.directory, model.printed, model.author,index)
             data.append(single_entry)
         return data
 
